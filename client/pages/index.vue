6e4f618794214d2e21e0f3cb21e06fb6f3fd369f
w3cdp6084-dev <yusukew3@gmail.com>
2022-06-02 20:05:52 +0900

    code

---
+++ client/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { data } = await useFetch("/blog", {
+  baseURL: "https://koneru-collection.microcms.io/api/v1",
+  headers: {
+    "X-MICROCMS-API-KEY": "a018dfee22ac488b98d4730036ada92571ee",
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>Hello, Nuxt3!</h1>
+    <ul>
+      <li v-for="article in data.contents" :key="article.id">
+        <nuxt-link :to="`/${article.id}`">{{ article.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
